6397fc1fbe73f87e81215e282c38fe1bd76f9ae9
ChetanXpro <chetanbaliyan10@gmail.com>
2022-11-23 12:11:14 +0530

    Feat: small fix

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import { ScanCommand } from "@aws-sdk/client-dynamodb";
 import { ddbClient } from "./ddbClient.js";
 
+import fs from "fs";
+
 const params = {
   ProjectionExpression: "id, parentChapterId",
 
-  TableName: "TABLE_NAME",
+  TableName: "Chapter-TABLE_NAME",
 };
 
 export const run = async () => {
   let chapterIds = [];
   let parentchapterIds = [];
   let orphan = [];
+  let grandchild = [];
+  let greatgrandchild = [];
 
   try {
     // Fetch all data related (id and parentChapterId)
@@ -28,31 +32,47 @@ export const run = async () => {
       (item) => item.parentChapterId.S !== "none"
     );
 
+    // This will find chapter direct under root chapter (son)  (Result => 36 orphan chapters)
+
     rootElementRemoved.forEach((item) => {
       if (!chapterIds.includes(item.parentChapterId.S)) {
         orphan.push(item.id.S);
       }
     });
 
-    // run this func one more time if we get some orphan chapters
+    //  run this func one more time if we get some orphan chapters
 
-    if (orphan.length !== 0) {
+    // 1st approach , but here func will run 2 or 3 times
+
+    /* if (orphan.length !== 0) {
       run();
     } else {
       return console.log("All orphan Chapters deleted");
-    }
+    } */
 
-    // We can get all childrens but here we need to run this some more times
+    // 2nd approach
 
-    // parentchapterIds.forEach((item) => {
-    //   if (orphan.some((i) => i === item.parentChapterId.S)) {
-    //     orphan.push(item.id.S);
+    // In this approach we can get all orphan chapters in one go.
 
-    //     console.log(item);
-    //   }
-    // });
+    // This will find chapter  grandSon chapters (Result => 117 orphan chapters)
+
+    parentchapterIds.forEach((item) => {
+      if (orphan.some((i) => i === item.parentChapterId.S)) {
+        grandchild.push("grand" + item.id.S);
+      }
+    });
+
+    //  This will find   greatgrandSon chapters but here we got 0 results
+
+    parentchapterIds.forEach((item) => {
+      if (grandchild.some((i) => i === item.parentChapterId.S)) {
+        greatgrandchild.push("great" + item.id.S);
+      }
+    });
 
     console.log("orphan--- " + orphan.length);
+    console.log("orphan2--- " + grandchild.length);
+    console.log("orphan-3-- " + greatgrandchild.length);
   } catch (err) {
     console.error(err);
   }
